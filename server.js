889546b3e158af4cf2dc04293a4818ce9458da9a
Diego López <noctuner16@gmail.com>
2024-05-02 13:34:57 -0600

    se sigue intentando, se vinculó la base de datos uwu

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const router = require('./routes/routes');
+
+// Configuración del motor de plantillas Pug
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.use('/', router);
+
+
+// Puerto en el que escucha el servidor
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
